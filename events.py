832b3fd2abc6fff8af194950876f0ddddc87a456
snowham <51470282+snowham@users.noreply.github.com>
2020-04-26 11:41:45 -0700

    Add files via upload

---
+++ events.py
@@ -0,0 +1,16 @@
+import sys
+sys.path.append('/home/runner/Coding4EverybodyGame/classes')
+import entity
+import pygame
+import reference
+
+
+def Setup():
+    pass
+
+
+def Start():
+    pass
+
+
+# What is this module supposed to do?????? We already have an events handler in main.py. The events handler is from pygame.
